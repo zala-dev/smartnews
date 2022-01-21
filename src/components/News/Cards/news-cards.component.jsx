246174a5b22a86bb730ead7a46fae2d3c64b6fa6
Bhagirath Zala <zala.developer@gmail.com>
2022-01-21 20:59:50 +1030

    uninstalled  words-to-numbers package

--- src/components/News/Cards/news-cards.component.jsx
+++ src/components/News/Cards/news-cards.component.jsx
@@ -5,21 +5,21 @@ import NewsCard from '../Card/news-card.component';
 import useStyles from './news-cards.styles.js';
 
 const infoCards = [
-  { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
+  { color: '#000000', title: 'Latest News', text: 'Give me the latest news' },
   {
-    color: '#1565c0',
+    color: '#0c1821',
     title: 'News by Categories',
     info: 'Business, Entertainment, General, Health, Science, Sports, Technology',
     text: 'Give me the latest Technology news',
   },
   {
-    color: '#4527a0',
+    color: '#1b2a41',
     title: 'News by Terms',
     info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...',
     text: "What's up with PlayStation 5",
   },
   {
-    color: '#283593',
+    color: '#324a5f',
     title: 'News by Sources',
     info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...',
     text: 'Give me the news from BBC News',
